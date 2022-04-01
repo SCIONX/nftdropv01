032741e8923197291c9022771655213aa7004c40
SCIONX <85130359+SCIONX@users.noreply.github.com>
2022-04-01 15:56:25 -0600

    add new

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,8 +2,16 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 
 import AiOutlineHome from '@react-icons/all-files/ai/AiOutlineHome'
 import GoHome from '@react-icons/all-files/go/GoHome'
+import { GetServerSideProps } from 'next'
+import { sanityClient, urlFor } from '../../sanity'
+import { Collection } from '../../typing'
+import Link from 'next/link'
 
-function NFTDropPage() {
+interface Props {
+  collection: Collection
+}
+
+function NFTDropPage({ collection }: Props) {
   //Auth
   const connectWithMetamask = useMetamask()
   const address = useAddress()
@@ -16,7 +24,7 @@ function NFTDropPage() {
     <div className="z-10 flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left side */}
 
-      <div className="bg-gradient-to-br from-cyan-900 to-red-400 lg:col-span-4">
+      <div className="bg-gradient-to-br from-cyan-900 to-green-700 lg:col-span-4">
         <div className="absolute left-2 top-2 hover:animate-pulse">
           <a title="Homepage" className="text-3xl text-gray-200" href="/">
             ⇦
@@ -25,19 +33,17 @@ function NFTDropPage() {
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="relative rounded-xl bg-gradient-to-br from-yellow-400 to-cyan-600 p-2">
             <img
-              className="w-44 rounded-xl object-cover transition duration-300 ease-in-out hover:animate-rotate-y lg:h-96 lg:w-72"
-              src="https://links.papareact.com/8sg"
+              className="w-44 rounded-xl object-cover hover:animate-rotate-y lg:h-96 lg:w-72"
+              src={urlFor(collection.previewImage).url()}
               alt=""
             />
           </div>
           <div className="space-y-2 p-5 text-center">
             <h1 className="text-4xl font-bold text-white">
-              <span className="animate-bounce">P</span>APAFAM Apes
+              {collection.nftCollectionName}
             </h1>
 
-            <h2 className="text-xl text-gray-300">
-              A collection of PAPAFAM Apes who live & breathe React
-            </h2>
+            <h2 className="text-xl text-gray-300">{collection.description}</h2>
           </div>
         </div>
       </div>
@@ -46,17 +52,19 @@ function NFTDropPage() {
       <div className="flex flex-1 flex-col p-12 lg:col-span-6">
         {/* Header */}
         <header className="flex items-center justify-between">
-          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
-            The{' '}
-            <span className="font-extrabold underline decoration-pink-600/50">
-              PAPAFAM
-            </span>{' '}
-            NFT MARKET Place
-          </h1>
+          <Link href={'/'}>
+            <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+              The{' '}
+              <span className="font-extrabold underline decoration-pink-600/50">
+                PAPAFAM
+              </span>{' '}
+              NFT MARKET Place
+            </h1>
+          </Link>
           <button
             title="Sign In/Out"
             onClick={() => (address ? disconnect() : connectWithMetamask())}
-            className="rounded-full bg-cyan-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
+            className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
           >
             {address ? 'Sign Out' : 'Sign In'}
           </button>
@@ -74,19 +82,19 @@ function NFTDropPage() {
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
           <img
             className="w-80 object-cover pb-10 lg:h-40"
-            src="https://links.papareact.com/bdy"
+            src={urlFor(collection.mainImage).url()}
             alt=""
           />
           <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
-            The PAPAFAM Ape Coding Club | NFT DROP
+            {collection.title}
           </h1>
-          <p className="pt-2 text-xl text-cyan-500">13 / 21 NFT's claimed</p>
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
         </div>
 
         {/* Mint Button */}
         <button
           title="Mint NFT"
-          className="mt-10 h-16 w-full rounded-full bg-cyan-700 font-bold text-white"
+          className="mt-10 h-16 w-full rounded-full bg-green-700 font-bold text-white"
         >
           Mint NFT (0.01 ETH)
         </button>
@@ -96,3 +104,46 @@ function NFTDropPage() {
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const query = `*[_type == "collection" && slug.current == $id][0]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+     asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+  current
+  },
+  creator -> {
+    _id,
+      name,
+      address,
+      slug {
+        current
+      },
+  },
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id,
+  })
+
+  if (!collection) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      collection,
+    },
+  }
+}
